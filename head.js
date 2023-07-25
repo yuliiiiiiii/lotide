af8814b289a27d0f971985609e00f483e7a7c3aa
Yuli Wang <yuliwang1007@gmail.com>
2023-07-24 21:51:37 -0400

    assertEqual and head function created

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `😁😁😁 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `😡😡😡 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head([]), undefined));
+console.log(assertEqual(head(["Hello", "Lighthouse", "labs"]), "Hello"));
