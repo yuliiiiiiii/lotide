5047f403ed62337f05bcb53566d49e9939d7b13c
Yuli Wang <yuliwang1007@gmail.com>
2023-07-26 14:18:42 -0400

    created assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if(array1[i] !== array2[i]) {
+      console.log(`😡😡😡 Assertion Failed: ${array1} !== ${array2}`);
+      return;
+      // to exit the function
+    }
+  }
+  console.log(`😁😁😁 Assertion Passed: ${array1} === ${array2}`);
+}
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+console.log(assertArraysEqual(['1', '2', '3'], ['1', '2', '3']));
+console.log(assertArraysEqual(['1', '2', '3'], ['1', '2', 3]));
