08d47c8df758cb8d84ba86e7b802edb5c5755b22
Yuli Wang <yuliwang1007@gmail.com>
2023-07-26 13:45:55 -0400

    add eqArrays function

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `😁😁😁 Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `😡😡😡 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
