4586a5a3b7603bd0ee75d57010d89b74c7f77c7b
Yuli Wang <yuliwang1007@gmail.com>
2023-07-27 23:52:53 -0400

    created takeUntil function

---
+++ takeUntil.js
@@ -0,0 +1,29 @@
+const takeUntil = function(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) {
+      return array.slice(0, i);
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+const assertArraysEqual = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`😡😡😡 Assertion Failed: ${array1} !== ${array2}`);
+      return;
+      // to exit the function
+    }
+  }
+  console.log(`😁😁😁 Assertion Passed: ${array1} === ${array2}`);
+};
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
