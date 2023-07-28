33b626958e91aac5f59e651556a6aa94d22bffc4
Yuli Wang <yuliwang1007@gmail.com>
2023-07-27 21:40:53 -0400

    created map function

---
+++ map.js
@@ -0,0 +1,40 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+};
+
+const results1 = map(words, word => word[0]);
+const results2 = map(words, word => word[1]);
+const results3 = map(words, word => word.length);
+console.log(results1);
+console.log(results2);
+console.log(results3);
+
+const assertArraysEqual = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`😡😡😡 Assertion Failed: ${array1} !== ${array2}`);
+      return;
+    }
+  }
+  console.log(`😁😁😁 Assertion Passed: ${array1} === ${array2}`);
+};
+
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+assertArraysEqual(results1, ["g", "c", "t", "m", "t"]);
+assertArraysEqual(results2, ["r", "o", "o", "a", "o"]);
+assertArraysEqual(results3, [6, 7, 2, 5, 3]);
