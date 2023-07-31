5064efc8d565bdcaa87271309aaad68bbb5563a1
Yuli Wang <yuliwang1007@gmail.com>
2023-07-31 17:36:02 -0400

    added eqArrayTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return `😁😁😁 Assertion Passed: ${actual} === ${expected}`;
-  } else {
-    return `😡😡😡 Assertion Failed: ${actual} !== ${expected}`;
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -18,7 +10,4 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false));
