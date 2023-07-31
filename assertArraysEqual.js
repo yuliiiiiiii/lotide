a3416391543d2feaf784256578f894892fa02e25
Yuli Wang <yuliwang1007@gmail.com>
2023-07-31 17:48:44 -0400

    added assertArrayEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
@@ -19,7 +9,5 @@ const assertArraysEqual = function(array1, array2) {
   }
 }
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
-console.log(assertArraysEqual(['1', '2', '3'], ['1', '2', '3']));
-console.log(assertArraysEqual(['1', '2', '3'], ['1', '2', 3]));
+module.exports = assertArraysEqual;
+
---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+console.log(assertArraysEqual(['1', '2', '3'], ['1', '2', '3']));
+console.log(assertArraysEqual(['1', '2', '3'], ['1', '2', 3]));
